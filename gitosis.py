2c4afefc92e381fb8a2e5aed09f88ca7c6498752
jlew <jlew.blackout@gmail.com>
2011-09-03 11:01:26 -0400

    Put public domain as is header

--- gitosis.py
+++ gitosis.py
@@ -1,3 +1,15 @@
+#########################################################################
+## This software is in the public domain, furnished "as is", without   ##
+## technical support, and with no warranty, express or implied, as to  ##
+## its usefulness for any purpose.                                     ##
+##                                                                     ##
+## gitosis.py                                                          ##
+## A simple script for managing gitosis config files                   ##
+##                                                                     ##
+## https://gitorious.org/jlew/gitosis-config                           ##
+##                                                                     ##
+## Author: Justin Lewis <jlew.blackout@gmail.com>                      ##
+#########################################################################
 import ConfigParser
 
 class gitosis:
